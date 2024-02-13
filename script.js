9ae31167b0f7a55b5a6b757c28c81735f6b1d957
Dhruv Mehta <Dhruv.Mehta@finova.tech>
2024-02-13 10:24:07 +0530

    Started DOM.

---
+++ script.js
@@ -0,0 +1,28 @@
+// For Loop
+
+// From i = 1 - 10
+for(i=1; i<11; i++){
+    console.log(i)
+}
+
+console.log("Break");
+
+// from j = 25 - 34
+for(j=25; j<=34; j++){
+    console.log(j)
+}
+
+console.log("Break");
+
+
+// from k = 40 - 55
+for(k=40; k<=55; k++){
+    console.log(k)
+}
+
+console.log("Break");
+
+// from l = 40 - 3
+for(l=40; l>=1; l-- ){
+    console.log(l)
+}
